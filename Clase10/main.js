e8f3f5c3508dbf9088fc9c5238d30822a9fb7fe0
LucasDeusich <130710219+LucasDeusich@users.noreply.github.com>
2023-05-21 22:39:05 -0300

    Add files via upload

---
+++ Clase10/main.js
@@ -0,0 +1,12 @@
+//Defino variables
+var nombre = "Lucas"
+var apellido = "Deusich"
+console.log (`${nombre} ${apellido}`)
+
+var i = 2
+var x = 3
+var suma = i + x
+console.log(suma)
+
+var j = suma + 10
+console.log (j + 5)
